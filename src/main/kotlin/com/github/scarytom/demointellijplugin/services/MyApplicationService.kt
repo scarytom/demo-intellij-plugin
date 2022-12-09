f30efb941a0f705a72eba9df0ab64f9207590c00
GitHub Action <action@github.com>
2022-12-09 11:08:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/scarytom/demointellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/scarytom/demointellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.scarytom.demointellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.scarytom.demointellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/scarytom/demointellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/scarytom/demointellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.scarytom.demointellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.scarytom.demointellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
